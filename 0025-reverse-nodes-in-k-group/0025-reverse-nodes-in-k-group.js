633899d9af50c87a9a2e698b76283c3843d5629f
vtu30110 <vtu30110@veltech.edu.in>
2026-08-18 14:48:25 +0530

    Remove comments from reverseKGroup function
    Removed comments and function definition for ListNode.

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.js
@@ -1,15 +1,3 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @param {number} k
- * @return {ListNode}
- */
 var reverseKGroup = function(head, k) {
     // Check if there are at least k nodes left starting from `node`
     let node = head;
