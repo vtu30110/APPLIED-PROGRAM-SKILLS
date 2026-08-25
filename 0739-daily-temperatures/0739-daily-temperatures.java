1a3e38bca7d18f2a7ce53c35784336982f4b83c7
vtu30110 <vtu30110@veltech.edu.in>
2026-08-25 14:24:53 +0530

    Time: 64 ms (45.74%), Space: 107.9 MB (7.51%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Current temperature is warmer than the temperature
+            // at the index on top of the stack
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
